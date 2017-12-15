78de7a5f01b08b4b50ea44acc20d135ce744dae6
Simon Lacroix <simon@miaou.be>
2017-12-16 00:19:00 +0100

    keep database.yml in git + fix database names

--- scripts/init-development-databases.sql
+++ scripts/init-development-databases.sql
@@ -1,4 +1,4 @@
-CREATE DATABASE devices_development;
-CREATE DATABASE devices_test;
+CREATE DATABASE kraken_development;
+CREATE DATABASE kraken_test;
 GRANT USAGE ON *.* to 'testio'@'%' identified by '0UVur9l93FOBGbShNN4g';
 GRANT ALL PRIVILEGES ON *.* TO 'testio'@'%';
